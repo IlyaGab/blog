7c455555c9321bb0fb38747a05bc186b232b5738
Ilya Hab <ilyagab1994@gmail.com>
2023-07-03 12:54:45 +0200

    feat(users): add table to users.component

--- src/pages/users/users.component.tsx
+++ src/pages/users/users.component.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import * as Styled from "./users.styles";
 import { useUsersApi } from "@core/store";
+import { dataTable } from "./users.data";
+
+// TODO: add search input
 
 const Users: React.FC = () => {
   const { getAllUsers, cleanUsers, isLoading, usersIds, usersMap } = useUsersApi();
@@ -15,14 +18,35 @@ const Users: React.FC = () => {
   if (isLoading) return <h1>Loading...</h1>;
 
   return (
-    <Styled.Wrapper>
-      {!!usersIds.length &&
-        usersIds.map((id) => (
-          <Styled.UsersLink key={id} to={`/users/${id}`}>
-            {usersMap[id].name}
-          </Styled.UsersLink>
+    <table>
+      <thead>
+        {dataTable.map((headerCell) => (
+          <tr key={headerCell.id}>
+            <th>{headerCell.id}</th>
+            <th>{headerCell.name}</th>
+            <th>{headerCell.username}</th>
+            <th>{headerCell.email}</th>
+            <th>{headerCell.phone}</th>
+            <th>{headerCell.website}</th>
+          </tr>
         ))}
-    </Styled.Wrapper>
+      </thead>
+      <tbody>
+        {!!usersIds.length &&
+          usersIds.map((id) => (
+            <tr key={id}>
+              <td>{usersMap[id].id}</td>
+              <td>
+                <Styled.UsersLink to={`${id}`}>{usersMap[id].name}</Styled.UsersLink>
+              </td>
+              <td>{usersMap[id].username}</td>
+              <td>{usersMap[id].email}</td>
+              <td>{usersMap[id].phone}</td>
+              <td>{usersMap[id].website}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
   );
 };
 
---
+++ src/pages/users/users.data.ts
@@ -0,0 +1,12 @@
+export const dataTable: Table[] = [
+  { id: "id", name: "name", username: "username", email: "email", phone: "phone", website: "website" },
+];
+
+interface Table {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}

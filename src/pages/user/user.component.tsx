7b9afe86b4c4d55deae6fda811fdda53cf01dfa5
Ilya Hab <ilyagab1994@gmail.com>
2023-07-21 17:47:09 +0200

    feature(user): add user information and styles

--- src/pages/user/user.component.tsx
+++ src/pages/user/user.component.tsx
@@ -12,13 +12,17 @@ enum Tabs {
 }
 
 const UserPage: React.FC = () => {
-  const { getUserById, cleanCurrentUser, isLoading } = useUsersApi();
+  const { getUserById, cleanCurrentUser, isLoading, currentUser } = useUsersApi();
   const { id } = useParams();
-  const [currentTab, setCurrentTab] = useState(Tabs.Posts);
+  const [currentTab, setCurrentTab] = useState("");
 
   const onTabClick = (tab: Tabs) => () => {
     setCurrentTab(tab);
   };
+
+  useEffect(() => {
+    setCurrentTab(Tabs.Posts);
+  }, []);
 
   useEffect(() => {
     if (id) {
@@ -34,15 +38,34 @@ const UserPage: React.FC = () => {
   return (
     <>
       <Styled.Wrapper>
-        <Button onClick={onTabClick(Tabs.Posts)} variant="text">
-          Posts
-        </Button>
-        <Button onClick={onTabClick(Tabs.Todos)} variant="contained">
-          Todos
-        </Button>
-        <Button onClick={onTabClick(Tabs.Todos)} variant="outlined">
-          Todos
-        </Button>
+        <Styled.Container>
+          <Styled.Title>User Information</Styled.Title>
+          <Styled.Box>
+            <Styled.Info>
+              <div>User id - {currentUser?.id}</div>
+              <div>User name - {currentUser?.name}</div>
+              <div>User username - {currentUser?.username}</div>
+              <div>User email - {currentUser?.email}</div>
+            </Styled.Info>
+            <Styled.Address>
+              User Address:
+              <div>City name - {currentUser?.address.city}</div>
+              <div>Street name - {currentUser?.address.street}</div>
+              <div>Zip-code - {currentUser?.address.zipcode}</div>
+            </Styled.Address>
+          </Styled.Box>
+        </Styled.Container>
+        <div>
+          <Button onClick={onTabClick(Tabs.Posts)} variant="text">
+            Posts
+          </Button>
+          <Button onClick={onTabClick(Tabs.Todos)} variant="contained">
+            Todos
+          </Button>
+          <Button onClick={onTabClick(Tabs.Todos)} variant="outlined">
+            Todos
+          </Button>
+        </div>
       </Styled.Wrapper>
       {currentTab === Tabs.Posts && <PostsPage />}
       {currentTab === Tabs.Todos && <TodosPage />}
--- src/pages/user/user.styles.ts
+++ src/pages/user/user.styles.ts
@@ -3,4 +3,38 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+`;
+
+export const Container = styled.div`
+  margin: 20px auto;
+  padding: 10px;
+  width: 80%;
+  height: 30%;
+  min-height: 10rem;
+  border: 2px solid ${({ theme }) => theme.colors.backgrounds.primary};
+  border-radius: 5px;
+  ${({ theme }) => theme.fonts.primary[20]}
+`;
+
+export const Box = styled.div`
+  display: flex;
+  justify-content: space-around;
+  ${({ theme }) => theme.fonts.primary[20]}
+`;
+
+export const Title = styled.div`
+  padding: 10px 0px;
+  text-align: center;
+  ${({ theme }) => theme.fonts.primary[40]}
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Address = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
